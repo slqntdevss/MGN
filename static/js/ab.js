ee0e33772320cf9ebe73175b9f67d86384ba9d30
Omega Network Founder <94934670+GamingReborn@users.noreply.github.com>
2022-11-26 23:33:33 +0000

    update

--- static/js/ab.js
+++ static/js/ab.js
@@ -1,9 +1,10 @@
 function ab() {
 	alert("About Blank cloaking WILL NOT prevent blocking. It just makes it so the URL is hidden.")
- var page = new ABC({
-    "type": "blank",
-    "url": window.location.origin
-  })
-  console.log(page.getCode())
-  page.open()
+	var page = new ABC({
+		"type": "blank",
+		"url": window.location.origin
+	})
+	console.log(page.getCode())
+	page.open()
+	location.replace("https://classroom.google.com")
 }
--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,9 @@
-importScripts('/uv.bundle.js');
-importScripts('/uv.config.js');
-importScripts('/uv.sw.js');
+importScripts('./uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener('fetch', event =>
+    event.respondWith(
+        sw.fetch(event)
+    )
+);
